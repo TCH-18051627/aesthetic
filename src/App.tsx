0a9ea6da10539b5c94f05dd769c074647f0e16b0
Oh丶jack <1045474326@qq.com>
2022-03-05 11:20:50 +0800

    feat: 登录页面代码修订

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  const isLogin = false;
+  const isLogin = true;
   return <Router>{isLogin ? <HomePage /> : <LoginPage />}</Router>;
 }
 
