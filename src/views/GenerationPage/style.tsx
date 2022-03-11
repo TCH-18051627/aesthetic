5a587a0500b03c32e2bb7a672fbd7fc3686ccfa6
zhou jie <1056792863@qq.com>
2022-03-11 18:08:37 +0800

    feat:美学评价页面

--- src/views/GenerationPage/style.tsx
+++ src/views/GenerationPage/style.tsx
@@ -1,1 +1,35 @@
 import styled from 'styled-components';
+import { Button, InputNumber, Dropdown, Card } from 'antd';
+
+export const GenerateWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+// 选择下拉框是按钮
+export const SelectButton = styled(Button)``;
+
+// 选择生成类型框
+export const TypeDropdown = styled(Dropdown)`
+  margin: 0 20px;
+  width: 150px;
+`;
+// 选择生成模型框
+export const ModelDropdown = styled(Dropdown)`
+  //margin-left: 0px;
+  width: 150px;
+`;
+
+export const GenerateButton = styled(Button)`
+  margin: 0 20px;
+  width: 100px;
+`;
+// 生成图片数量
+export const ImageInputNumber = styled(InputNumber)`
+  margin-left: 60px;
+  width: 260px;
+`;
+
+export const ImgCard = styled(Card)`
+  width: 100%;
+  text-align: center;
+`;
---
+++ src/views/GenerationPage/testData/data.tsx
@@ -0,0 +1,36 @@
+import image1 from '@/assets/images/image1.jpg';
+import image2 from '@/assets/images/image2.jpg';
+import image3 from '@/assets/images/image3.jpg';
+
+export const imgSource = [
+  {
+    imageId: 'image1',
+    imageUrl: image1,
+    imageScore: 0.65
+  },
+  {
+    imageId: 'image2',
+    imageUrl: image2,
+    imageScore: 0.65
+  },
+  {
+    imageId: 'image3',
+    imageUrl: image3,
+    imageScore: 0.65
+  },
+  {
+    imageId: 'image1',
+    imageUrl: image1,
+    imageScore: 0.65
+  },
+  {
+    imageId: 'image2',
+    imageUrl: image2,
+    imageScore: 0.65
+  },
+  {
+    imageId: 'image3',
+    imageUrl: image3,
+    imageScore: 0.65
+  }
+];
